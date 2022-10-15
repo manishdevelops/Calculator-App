2de5ef095c4b76c8811ad64d4ad0439015bd18ad
manish mandal <76776194mm@gmail.com>
2022-10-15 17:23:22 +0530

    update js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,26 @@
 'use strict'
 
 const switchOpts=document.querySelectorAll('.btnOptions');
+const totalSwitches=switchOpts.length;
+let currSwitch;
+
+const slideBtn=function(val){
+  let slideValue=0;
+  if(totalSwitches > currSwitch)
+    slideValue=val-totalSwitches;
+  else
+    slideValue=totalSwitches - val;
+  console.log(slideValue);
+}
 
 switchOpts.forEach(function(ele){
   ele.addEventListener('click',function(e){
     console.log(1);
     if (e.target.classList.contains('btnOptions')){
       const TargetBtn=e.target.dataset.index;
+      currSwitch=TargetBtn;
       console.log(TargetBtn);
-      // const dataSet=
+      slideBtn(TargetBtn);
     }
     return;
   })
