6b31706de67b260a17d4fac41bbeb9c68f5cea34
manish mandal <76776194mm@gmail.com>
2022-10-19 16:33:37 +0530

    update js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
 'use strict'
-import variables from '/sass/_variables.scss';
 
 const switchOpts=document.querySelectorAll('.btnOptions');
 const toggleSwitchBtn=document.querySelector('.toggleBtn');
-// const Body = document.getElementsByTagName('BODY')[0];
-// console.log(Body);
+
 
 const totalSwitches=switchOpts.length;
 let currSwitch=1;
@@ -15,11 +13,9 @@ const goToBtn=function(val){
 
 switchOpts.forEach(function(ele){
   ele.addEventListener('click',function(e){
-    // console.log(1);
     if (e.target.classList.contains('btnOptions')){
       const TargetBtn=+ e.target.dataset.index;
       console.log(typeof TargetBtn);
-     
       goToBtn(TargetBtn);
       currSwitch=TargetBtn;
     }
